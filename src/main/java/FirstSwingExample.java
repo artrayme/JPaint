699e41645937e4ee9377e0964f92e6caef6c3e00
artrayme <artrayme@protonmail.com>
2021-02-11 04:12:15 +0300

    tab bar, tool bar, icons for tool bar

--- src/main/java/FirstSwingExample.java
+++ src/main/java/FirstSwingExample.java
@@ -1,13 +1,31 @@
 import MainWindow.MainWindow;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Enumeration;
 
 public class FirstSwingExample {
+
     public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) (screenSize.getWidth()*0.8);
+        int height = (int)(screenSize.getHeight()*0.8);
+
+        setUIFont(new javax.swing.plaf.FontUIResource("Times New Roman",Font.BOLD,14));
+
         MainWindow mainWindow = new MainWindow();
-        mainWindow.setBounds(100, 100, 400, 500);
-        mainWindow.setLayout(null);
-        mainWindow.setVisible(true);
+        mainWindow.setBounds((screenSize.width-width)/2, (screenSize.height-height)/2, width, height);
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setVisible(true);
     }
-}  
+
+    public static void setUIFont (javax.swing.plaf.FontUIResource f){
+        Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get (key);
+            if (value instanceof javax.swing.plaf.FontUIResource)
+                UIManager.put (key, f);
+        }
+    }
+}
--- src/main/java/MainWindow/MainWindow.java
+++ src/main/java/MainWindow/MainWindow.java
@@ -1,14 +1,118 @@
 package MainWindow;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
     private final MenuBar menuBar = new MenuBar();
     private final JTabbedPane tabBar = new JTabbedPane();
+    private final JPanel toolBar = new JPanel();
+    private final JColorChooser colorChooser = new JColorChooser();
 
     public MainWindow() {
+        setLayout(new BorderLayout());
+        initMenuBar();
+        initToolBar();
+        initTabBar();
         this.setJMenuBar(menuBar);
-        this.add(tabBar);
-        tabBar.add(new DrawArea());
+        this.add(toolBar, BorderLayout.NORTH);
+        this.add(tabBar, BorderLayout.CENTER);
     }
+
+    private void initMenuBar() {
+
+    }
+
+    private void initTabBar() {
+        tabBar.add("untiled1", new DrawArea());
+        tabBar.add("untiled2", new DrawArea());
+        tabBar.add("untiled3", new DrawArea());
+    }
+
+    private void initToolBar() {
+        toolBar.setLayout(new BoxLayout(toolBar, BoxLayout.X_AXIS));
+        JButton pencil = initPencilButton();
+        JButton line = initLineButton();
+        JButton rectangle = initRectangleButton();
+        JButton ellipse = initEllipseButton();
+        JButton erase = initEraseButton();
+        JButton colors = initColorsButton();
+        JButton selection = initSelectionButton();
+        JButton lasso = initLassoButton();
+        toolBar.add(pencil);
+        toolBar.add(line);
+        toolBar.add(rectangle);
+        toolBar.add(ellipse);
+        toolBar.add(new JToolBar.Separator());
+        toolBar.add(erase);
+        toolBar.add(new JToolBar.Separator());
+        toolBar.add(colors);
+        toolBar.add(new JToolBar.Separator());
+        toolBar.add(selection);
+        toolBar.add(lasso);
+    }
+
+    private ImageIcon initButtonIcon(String path) {
+        Image dimg = null;
+        try {
+            Image img = ImageIO.read(getClass().getResource(path));
+            dimg = img.getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        assert dimg != null;
+        return new ImageIcon(dimg);
+    }
+
+    private JButton initPencilButton() {
+        JButton pencil = new JButton();
+        pencil.setIcon(initButtonIcon("/icons/pencil.png"));
+        return pencil;
+    }
+
+    private JButton initLineButton() {
+        JButton line = new JButton();
+        line.setIcon(initButtonIcon("/icons/line.png"));
+        return line;
+    }
+
+    private JButton initEllipseButton() {
+        JButton ellipse = new JButton();
+        ellipse.setIcon(initButtonIcon("/icons/ellipse.png"));
+        return ellipse;
+    }
+
+    private JButton initRectangleButton() {
+        JButton rectangle = new JButton();
+        rectangle.setIcon(initButtonIcon("/icons/rectangle.png"));
+        return rectangle;
+    }
+
+    private JButton initEraseButton() {
+        JButton erase = new JButton();
+        erase.setIcon(initButtonIcon("/icons/erase.png"));
+        return erase;
+    }
+
+    private JButton initColorsButton() {
+        JButton colors = new JButton();
+        colors.setIcon(initButtonIcon("/icons/color.png"));
+        return colors;
+    }
+
+    private JButton initSelectionButton() {
+        JButton selection = new JButton();
+        selection.setIcon(initButtonIcon("/icons/selection.png"));
+        return selection;
+    }
+
+
+    private JButton initLassoButton() {
+        JButton lasso = new JButton();
+        lasso.setIcon(initButtonIcon("/icons/lasso.png"));
+        return lasso;
+    }
+
+
 }
